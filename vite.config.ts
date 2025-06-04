d9cc267e9857594a74ec700d9e502fc9c839ce84
iriio <106037695+iriio@users.noreply.github.com>
2025-06-04 19:46:41 -0400

    Configure: Set base path to / for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig(({ command }) => ({
-  base: command === "serve" ? "/" : "/Assortment-Planner/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
