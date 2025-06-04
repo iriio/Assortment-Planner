da681a8ac584bb4ca3f3026fc0c70424b1f0bd69
iriio <106037695+iriio@users.noreply.github.com>
2025-06-04 19:22:51 -0400

    Fix: Update base path and redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig(({ command }) => ({
-  base: command === "serve" ? "/" : "/assortment-planner/",
+  base: command === "serve" ? "/" : "/Assortment-Planner/",
   plugins: [react()],
   resolve: {
     alias: {
